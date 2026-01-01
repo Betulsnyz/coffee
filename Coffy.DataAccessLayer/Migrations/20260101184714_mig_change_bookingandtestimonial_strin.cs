72237b4a1b36a9701381262adc658452d22824f1
Betulsnyz <snyzbetul@gmail.com>
2026-01-01 22:17:03 +0300

    TestimonialcVe Booking tablosu değişikliği

---
+++ Coffy.DataAccessLayer/Migrations/20260101184714_mig_change_bookingandtestimonial_strin.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Coffy.DataAccessLayer.Migrations
+{
+    public partial class mig_change_bookingandtestimonial_strin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
