596cd5c489b05a9a6f6898d7f4713da6d6d64120
Betulsnyz <snyzbetul@gmail.com>
2026-01-06 00:54:46 +0300

    basket kısmının devamı
    silme eksik

--- CoffyWebUI/Controllers/MenuController.cs
+++ CoffyWebUI/Controllers/MenuController.cs
@@ -1,6 +1,8 @@
-﻿using CoffyWebUI.Dtos.ProductDtos;
+﻿using CoffyWebUI.Dtos.BasketDtos;
+using CoffyWebUI.Dtos.ProductDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace CoffyWebUI.Controllers
 {
@@ -23,6 +25,21 @@ namespace CoffyWebUI.Controllers
             var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsondata);
             return View(values);
 
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddBasket(int id)
+        {
+            CreateBasketDto createBasketDto = new CreateBasketDto();
+            createBasketDto.ProductID = id;
+            var client = _httpClientFactory.CreateClient();
+            var jsondata = JsonConvert.SerializeObject(createBasketDto);
+            StringContent stringContent = new StringContent(jsondata, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7113/api/Basket", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return Json(createBasketDto);
         }
     }
 }
