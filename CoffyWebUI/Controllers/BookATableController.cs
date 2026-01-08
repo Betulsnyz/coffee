05fd63a27b97e46ea2234a9be9500c736ac8f58b
Betulsnyz <snyzbetul@gmail.com>
2026-01-08 22:02:10 +0300

    rezervasyon signalr bağlantısı tamamlandı

---
+++ CoffyWebUI/Controllers/BookATableController.cs
@@ -0,0 +1,38 @@
+﻿using CoffyWebUI.Dtos.BookingDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace CoffyWebUI.Controllers
+{
+    public class BookATableController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookATableController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateBookingDto createBookingDto)
+        {
+            createBookingDto.PersonCount = 2;
+            var client = _httpClientFactory.CreateClient();
+            var jsondata = JsonConvert.SerializeObject(createBookingDto);
+            StringContent stringContent = new StringContent(jsondata, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7113/api/Booking", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index","Default");
+            }
+            return View();
+        }
+    }
+}
