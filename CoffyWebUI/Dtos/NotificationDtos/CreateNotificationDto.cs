478385c999c0ef5b569c79a8f4f5491ad595865d
Betulsnyz <snyzbetul@gmail.com>
2026-01-12 23:43:00 +0300

    bildirime birkaç eklenti

--- CoffyWebUI/Dtos/NotificationDtos/CreateNotificationDto.cs
+++ CoffyWebUI/Dtos/NotificationDtos/CreateNotificationDto.cs
@@ -11,7 +11,6 @@ namespace CoffyWebUI.Dtos.NotificationDtos
         public string Type { get; set; }
         public string Icon { get; set; }
         public string Description { get; set; }
-        public DateTime Date { get; set; }
         public bool Status { get; set; }
     }
 }
