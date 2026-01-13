b6813b3e1de21740489bdbb3e09e785764f59e04
Betulsnyz <snyzbetul@gmail.com>
2026-01-13 22:18:51 +0300

    signal r ile anlık client sayısı getirme

---
+++ CoffyWebUI/Controllers/MessageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoffyWebUI.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ClientUserCount() 
+        {
+            return View();
+        }
+
+    }
+}
